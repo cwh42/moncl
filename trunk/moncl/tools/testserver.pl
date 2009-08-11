1ad9775a687a44d1d9abb16855260ef9dfe41d61
cwh <cwh@53118c76-b048-11df-9244-49f539f68fc7>
2009-08-11 12:44:56 +0000

    - more obvious variable naming - printing message when sending SMS - reformatted date string - moved one of the phone numbers out of default loop - started very primitive testing server for easier debugging

---
+++ trunk/moncl/tools/testserver.pl
@@ -0,0 +1,26 @@
+#!/usr/bon/perl -w
+
+use strict;
+use IO::Socket;
+
+my $server_port = 9333;
+
+my $server = IO::Socket::INET->new(LocalPort => $server_port,
+                                   Type => SOCK_STREAM,
+                                   Reuse => 1,
+                                   Listen => 10 )
+    or die("Cannot open port $server_port: $@\n");
+
+my %loop = ( loop => '23152',
+             channel => 1 );
+
+while (my $client = $server->accept())
+{
+    foreach (0,2)
+    {
+        print $client join(':', 300, time(), @loop{qw(channel loop)}, $_, 'msg')."\r\n";
+        sleep(1);
+    }
+}
+
+close($server);
