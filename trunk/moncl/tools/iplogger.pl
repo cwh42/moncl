ad34acd685413a2446d97eee8f79f63e7e90fda5
cwh <cwh@53118c76-b048-11df-9244-49f539f68fc7>
2009-10-16 14:28:11 +0000

    Touch file only if IP really has changed.

--- trunk/moncl/tools/iplogger.pl
+++ trunk/moncl/tools/iplogger.pl
@@ -2,16 +2,17 @@
 
 use strict;
 
-if( $ARGV[0] && $ARGV[0] eq 'set' )
+open(FILE, "</tmp/srvaddr");
+my $addr = <FILE> || '127.0.0.1';
+close(FILE);
+
+if( $ARGV[0] && $ARGV[0] eq 'set' && $ENV{'REMOTE_ADDR'} ne $addr )
 {
     open(FILE, ">/tmp/srvaddr");
     print FILE $ENV{'REMOTE_ADDR'};
     close(FILE);
 }
 
-open(FILE, "</tmp/srvaddr");
-my $addr = <FILE> || '127.0.0.1';
-close(FILE);
 
 print "Content-type: text/html\n\n";
 print "$addr\n";
