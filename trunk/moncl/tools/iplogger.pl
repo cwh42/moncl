06de494a1e168a430b6b12414139e5248e758c8d
cwh <cwh@53118c76-b048-11df-9244-49f539f68fc7>
2009-10-19 12:30:28 +0000

    Calling nsupdate on IP change

--- trunk/moncl/tools/iplogger.pl
+++ trunk/moncl/tools/iplogger.pl
@@ -2,19 +2,42 @@
 
 use strict;
 
+my $DNSUPDATE = 1;
+
+my $HOSTNAME = 'alarm.goessenreuth.de';
+my $TTL = 20*60;
+
 open(FILE, "</tmp/srvaddr");
 my $addr = <FILE> || '127.0.0.1';
 close(FILE);
 
 if( $ARGV[0] && $ARGV[0] eq 'set' && $ENV{'REMOTE_ADDR'} ne $addr )
 {
+    $addr = $ENV{'REMOTE_ADDR'};
+
     open(FILE, ">/tmp/srvaddr");
-    print FILE $ENV{'REMOTE_ADDR'};
+    print FILE $addr;
     close(FILE);
+
+    if($DNSUPDATE)
+    {
+        my $pid = open(NSUP, "|-", "nsupdate") or die("Could not fork: $!\n");
+
+        while( <main::DATA> )
+        {
+            s/(\$\w+)/$1/gee;
+            print NSUP $_; 
+        }
+
+        close(NSUP);
+    }
 }
 
 
 print "Content-type: text/html\n\n";
 print "$addr\n";
 
-
+__END__
+update delete $HOSTNAME A
+update add $HOSTNAME $TTL A $addr
+send
